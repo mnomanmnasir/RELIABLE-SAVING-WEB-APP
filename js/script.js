f33c8913afc62b4009ec7b8566bfe354eb1a35f6
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2023-03-30 07:43:09 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
+window.onscroll = () => {
+  navbar.classList.remove('active');
 }
 
-document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
-    btn.onclick = () =>{
-        let src = btn.getAttribute('data-src');
-        document.querySelector('.about .video-container .video').src = src;
-    }
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
+  btn.onclick = () => {
+    let src = btn.getAttribute('data-src');
+    document.querySelector('.about .video-container .video').src = src;
+  }
 })
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
